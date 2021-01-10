4dee1042dd4e66a5102ec13fc951d0aea36c3768
alestor123 <alestoraldous@gmail.com>
2021-01-10 18:12:15 +0530

    added main code

--- App.js
+++ App.js
@@ -3,4 +3,16 @@ var chalk = require('chalk'),
 pkgUp = require('pkg-up'),
 fs = require('fs'),
 axios = require('axios'),
-figlet = require('figlet');
+deps = (pkg,depType) => pkg[depType] && typeof pkg[depType] === 'object'? Object.keys(pkg[depType]): [] ;
+(async () => {
+const pkgPath = await pkgUp()
+fs.readFile(pkgPath, 'utf8', (err, data) => { 
+if(err) throw err
+var pkgs = JSON.parse(data),
+pcks =  [].concat(deps(pkgs,'dependencies'),deps(pkgs, 'devDependencies'));
+pcks.map(async name =>{
+var data = await axios.get(`http://registry.npmjs.com/${name}/latest`)
+console.log(chalk.red(name) + ' : ' + chalk.green(data.data.license))
+})})})();    
+
+    
